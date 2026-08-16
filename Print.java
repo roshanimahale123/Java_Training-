5e2c06e1669d9bdaecfd7981a0c5c097728a35c8
roshanimahale123 <your.email@example.com>
2026-08-16 19:24:26 +0530

    Initial commit of my learning files

--- Print.java
+++ Print.java
@@ -1,5 +1,7 @@
-class Print{
-    public static void main(String[ ] args){
-        System.out.println("Hello World");
+public class Print {
+    public static void main(String[] args) {
+        // args[0] holds the first argument, args[1] holds the second, etc.
+        System.out.println("First argument: " + args[0]);
+        System.out.println("Second argument: " + args[1]);
     }
 }
---
+++ TypeCasting.java
@@ -0,0 +1,10 @@
+public class TypeCasting {
+    public static void main(String[] args){
+        int a = 12990;
+        long b = a;
+        byte c = (byte) b;
+        System.out.println("Value of b: " + b);
+        System.out.println("Value of c: " + c);
+
+    }
+}
