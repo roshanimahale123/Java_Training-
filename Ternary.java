23efd13348a0478c586aef577eda8f2118183f03
roshanimahale123 <your.email@example.com>
2026-08-16 20:09:43 +0530

    Initiate

---
+++ Ternary.java
@@ -0,0 +1,8 @@
+public class Ternary {
+    public static void main(String[] args){
+        boolean isSwimming = true;
+
+        String result = isSwimming ? "I am swimming" : "I am not swimming";
+        System.out.println(result);
+    }
+}
